4e40445d7a6e5c2729e78949ded6af09e4c69d26
Aman Saeed <amantatla312@gmail.com>
2024-09-26 17:47:58 +0500

    design completion

--- app.js
+++ app.js
@@ -3,23 +3,31 @@ const email = document.querySelector(".email");
 const message = document.querySelector(".message");
 const Name = document.querySelector(".name");
 
+window.onload = function () {
+    window.scrollTo(0, 0);  // Scroll to top of the page
+};
+
 function Menu(e) {
     let list = document.querySelector('ul');
-e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
 }
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+};
 
 const sendEmail = () => {
     Email.send({
-        SecureToken : "effaf952-f5b4-482e-9b18-a4b72042ba1f",
-        Host : "smtp.elasticemail.com",
-        Username : "ashish-makes@gmail.com",
-        Password : "42BD017CE4A916FAC9D7484BA57DF6FA8C17",
-        To : 'ashish-makes@gmail.com',
-        From : email.value,
-        Subject : "Email from Portfolio",
-        Body : message.value
+        SecureToken: "YOUR_SECURE_TOKEN",
+        Host: "smtp.elasticemail.com",
+        Username: "amantatla312@gmail.com",
+        Password: "YOUR_PASSWORD",
+        To: 'amantatla312@gmail.com',
+        From: email.value,
+        Subject: "Email from Portfolio",
+        Body: message.value
     }).then(
-      message => alert(message)
+        message => alert(message)
     );
     reset()
 }
